405361ee66795055546e3e9029217fe6f1e4ed28
Josh-McFarlin <jmcfarlin6@gatech.edu>
2017-10-14 15:54:46 -0400

    add Twitter

--- utils/Tweets.py
+++ utils/Tweets.py
@@ -1,12 +1,6 @@
 import tweepy
 import config
 
-"""
-class Tweet:
-    def __init__(self):
-
-
-"""
 
 auth = tweepy.OAuthHandler(config.TW_CONSUMER_KEY, config.TW_CONSUMER_SECRET)
 auth.set_access_token(config.TW_ACCESS_TOKEN_KEY, config.TW_ACCESS_TOKEN_SECRET)
@@ -18,11 +12,16 @@ def get_tweets(user_name, count):
     user_tweets = api.user_timeline(screen_name=user_name, count=count)
     tweets = []
     for tweet in user_tweets:
+        tweet_urls = [i['url'] for i in tweet.entities['urls']]
         txt = tweet.text
-        for i in tweet.urls:
-            txt = txt.replace(i['url'], "")
-        tweets.append(txt)
+        time = tweet.created_at
+        time_format = time.strftime('%B %d')
+        for i in tweet_urls:
+            txt = txt.replace(i, "")
+        joined = "{author} on {time}: {txt}".format(author=user_name, time=time_format, txt=txt)
+        tweets.append(joined)
     return "\n".join(tweets)
 
 
-print(get_tweets('GeorgiaTech', 3))
+if __name__ == "__main__":
+    print(get_tweets('GeorgiaTech', 3))
--- utils/data.py
+++ utils/data.py
@@ -1,13 +1,15 @@
 import time
 import datetime
 
-
-dining = {'North Ave': {'Monday': '7 AM - 2 AM', 'Tuesday': '7 AM - 2 AM', 'Wednesday': '7 AM - 2 AM',
-                            'Thursday': '7 AM - 2 AM', 'Friday': '7 AM - 10 PM', 'Saturday': '10 AM - 10 PM',
-                            'Sunday': '10 AM - 2 PM'},
-              'Brittain': {'Monday': '7 AM - 8 PM', 'Tuesday': '7 AM - 8 PM', 'Wednesday': '7 AM - 8 PM',
-                           'Thursday': '7 AM - 8 PM', 'Friday': '7 AM - 3 PM', 'Saturday': 'CLOSED',
-                           'Sunday': '4 PM - 8 PM'}}
+dining = {'north ave': {'Monday': '7 AM - 2 AM', 'Tuesday': '7 AM - 2 AM', 'Wednesday': '7 AM - 2 AM',
+                        'Thursday': '7 AM - 2 AM', 'Friday': '7 AM - 10 PM', 'Saturday': '10 AM - 10 PM',
+                        'Sunday': '10 AM - 12 AM'},
+          'brittain': {'Monday': '7 AM - 8 PM', 'Tuesday': '7 AM - 8 PM', 'Wednesday': '7 AM - 8 PM',
+                       'Thursday': '7 AM - 8 PM', 'Friday': '7 AM - 3 PM', 'Saturday': 'CLOSED',
+                       'Sunday': '4 PM - 8 PM'},
+          'west village': {'Monday': '7 AM - 2 AM', 'Tuesday': '7 AM - 2 AM', 'Wednesday': '7 AM - 2 AM',
+                           'Thursday': '7 AM - 2 AM', 'Friday': '7 AM - 10 PM', 'Saturday': '8 AM - 10 PM',
+                           'Sunday': '8 AM - 2 AM'}}
 
 
 def isOpen(dining_hall):
@@ -20,10 +22,11 @@ def isOpen(dining_hall):
         times[t] = times[t].strip()
         seperate = times[t].split(" ")
         if t == 0:
-            opentime = datetime.datetime.strptime(seperate[0] + ":00",'%H:%M')
-            print("Opening: Now = " + str(datetime.datetime.now().time()) +"\nOpening at = " + str(opentime.time()))
+            opentime = datetime.datetime.strptime(seperate[0] + ":00", '%H:%M')
+            print("Opening: Now = " + str(datetime.datetime.now().time()) + "\nOpening at = " + str(opentime.time()))
             if datetime.datetime.now().time() < opentime.time():
-                return False, "{} closed at {} and will not be open until {}".format(dining_hall,times[0].strip(),times[1].strip())
+                return False, "{} closed at {} and will not be open until {}".format(dining_hall, times[0].strip(),
+                                                                                     times[1].strip())
         elif t == 1:
             close = int(seperate[0])
             if seperate[1] == "PM":
@@ -31,20 +34,35 @@ def isOpen(dining_hall):
             closetime = datetime.datetime.strptime(str(close) + ":00", '%H:%M')
             print("Closing: Now = " + str(datetime.datetime.now().time()) + "\nClosing at = " + str(closetime.time()))
             if datetime.datetime.now().time() > closetime.time():
-                return False, "{} closed at {} and will not be open until {}".format(dining_hall, times[0].strip(),times[1].strip())
-        return True, "{} is open now until {}".format(dining_hall,times[1].strip())
+                return False, "{} closed at {} and will not be open until {}".format(dining_hall, times[0].strip(),
+                                                                                     times[1].strip())
+        return True, "{} is open now until {}".format(dining_hall, times[1].strip())
 
 
 def diningOpen():
     openlist = []
-    if isOpen("North Ave")[0]:
+    if isOpen("north ave")[0]:
         openlist.append("North Ave")
-    if isOpen("Brittain")[0]:
+    if isOpen("brittain")[0]:
         openlist.append("Brittain")
-
+    if isOpen("west village")[0]:
+        openlist.append("West Village")
     if len(openlist) == 0:
         return "Nothing is open today"
     elif len(openlist) == 1:
         return "Only {} is open today".format(openlist[0])
     elif len(openlist) == 2:
         return "{} and {} are open today".format(openlist[0], openlist[1])
+
+
+def diningHours(hall):
+    times = dining[hall][time.strftime("%A")]
+    if times == "CLOSED":
+        return "{} is closed today".format(hall)
+    else:
+        times = times.split("-")
+    return "{} is open from {} to {}".format(hall, times[0], times[1])
+
+
+if __name__ == "__main__":
+    print(diningHours("brittain"))
